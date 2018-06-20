c0791426a2a54b6b3e628533643c2dd31cf6c6b9
Sergius <kfzadrser@gmail.com>
2018-06-20 23:36:43 +0300

    update(,package.json,webpack.config): add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path');
+
+let conf = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'main.js'
+	},
+	devServer: {
+		overlay: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
+
+module.exports = conf;
